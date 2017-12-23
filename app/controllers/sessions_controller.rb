235a5c17ba3b556c74647ea8a4054cadfa2f76f7
Nikola Novakovic <nikola.novakovic9@gmail.com>
2017-12-23 12:52:49 -0500

    WIP on photos upload

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email])
-    byebug
-    if user && user.authenticate(params[:password])
+    user = User.find_by(email: params[:user][:email])
+    if user && user.authenticate(params[:user][:password])
       session[:user_id] = user.id
-      redirect_to :photos_path, notice: 'Logged in!'
+      redirect_to :photos, notice: 'Logged in!'
     else
       flash.now.alert = 'Email or password is invalid'
       render :new
