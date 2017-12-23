300f2543ad16e5db01e73f073349afc29843000e
Nikola Novakovic <nikola.novakovic9@gmail.com>
2017-12-23 09:23:01 -0500

    Add user view

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,5 @@ require 'bcrypt'
 class User < ApplicationRecord
   has_many :comments
   has_many :photos
-  include BCrypt
-
-  def password
-    @password ||= Password.new(password_hash)
-  end
-
-  def password=(new_password)
-    @password = Password.create(new_password)
-  end
+  has_secure_password
 end
